ab4ae74bbd525eede34380256bc056f74396000b
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-02-16 14:54:34 +0100

    Bouton fix + Form

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-const audio = new Audio("docs/mario.mp3");
-const buttons = document.querySelectorAll("button");
+// const audio = new Audio("docs/mario.mp3");
+// const buttons = document.querySelectorAll("#marial");
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    audio.play();
-  });
-});
+// buttons.forEach(button => {
+//   button.addEventListener("click", () => {
+//     audio.play();
+//   });
+// });
+
+
+
+
+
+
+
+const audio = new Audio("docs/mario.mp3");
+
+$ ("#marial").click (function (){
+
+audio.play();
+
+}); 
