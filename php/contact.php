d0d03ea40ab3545a25ef61cdea2b3dfe00a48676
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-02-16 22:47:10 +0100

    last Modifs CSS

--- php/contact.php
+++ php/contact.php
@@ -1,7 +1,164 @@
-<?php 
+<?php
+
+// require ReCaptcha class
+
+require('recaptcha-master/src/autoload.php');
 
 
 
+// configure
+
+$from = 'Contact t-jack.fr <noreply@t-jack.fr>';
+
+$sendTo = 'T-jack <contact@t-jack.fr>';
+
+// $sendTo = 'dwy <dwygaming@gmail.com>';
+
+$subject = 'Nouveau message du formulaire de contact - t-jack.fr';
+
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
+
+$okMessage = 'Message envoyé, merci ! A très bientôt !';
+
+$errorMessage = 'Erreur, merci de réessayer.';
+
+$recaptchaSecret = '6LdYCcQZAAAAAPWljYt3ohRpCSas0sROJ4Y9okd-';
+
+
+
+// let's do the sending
+
+
+
+try
+
+{
+
+    if (!empty($_POST)) {
+
+
+
+        // validate the ReCaptcha, if something is wrong, we throw an Exception, 
+
+        // i.e. code stops executing and goes to catch() block
+
+        
+
+        if (!isset($_POST['g-recaptcha-response'])) {
+
+            throw new \Exception('ReCaptcha is not set.');
+
+        }
+
+
+
+        // do not forget to enter your secret key in the config above 
+
+        // from https://www.google.com/recaptcha/admin
+
+        
+
+        //$recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret, new \ReCaptcha\RequestMethod\CurlPost());
+
+        
+
+		$recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret);
+
+		
+
+        // we validate the ReCaptcha field together with the user's IP address
+
+        
+
+        $response = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+
+
+
+
+
+        if (!$response->isSuccess()) {
+
+            throw new \Exception('ReCaptcha was not validated.');
+
+        }
+
+        
+
+        // everything went well, we can compose the message, as usually
+
+        
+
+        $emailText = "You have new message from contact form\n=============================\n";
+
+
+
+        foreach ($_POST as $key => $value) {
+
+
+
+            if (isset($fields[$key])) {
+
+                $emailText .= "$fields[$key]: $value\n";
+
+            }
+
+        }
+
+        
+
+
+
+        $headers = array('Content-Type: text/plain; charset="UTF-8";',
+
+            'From: ' . $from,
+
+            'Reply-To: ' . $from,
+
+            'Return-Path: ' . $from,
+
+        );
+
+
+
+        mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+
+
+        $responseArray = array('type' => 'success', 'message' => $okMessage);
+
+    }
+
+}
+
+catch (\Exception $e)
+
+{
+
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+
+}
+
+
+
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+
+    $encoded = json_encode($responseArray);
+
+
+
+    header('Content-Type: application/json');
+
+
+
+    echo $encoded;
+
+}
+
+else {
+
+    echo $responseArray['message'];
+
+}
 
 
 
