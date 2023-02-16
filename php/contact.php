ed6a2af066221a56743a988a712b8d53e166a798
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-02-16 23:49:40 +0100

    Formulaire + Envoi de mail OK

--- php/contact.php
+++ php/contact.php
@@ -1,170 +1,105 @@
 <?php
+// https://github.com/google/recaptcha
 
-// require ReCaptcha class
+// require ReCaptcha class 
 
-require('recaptcha-master/src/autoload.php');
+
+require('../js/recaptcha-master/src/autoload.php');
 
 
 
 // configure
+$from = 'Contact mempoule cv antoni <noreply@mempoule.lol>';
+$sendTo = 'AntoniSDev <antoni.salomon1337@gmail.com>';
 
-$from = 'Contact t-jack.fr <noreply@t-jack.fr>';
+// $sendTo = 'anto <antoni.salomon1337@gmail.com>';
+$subject = 'Nouveau message du formulaire de contact - CV AntoniS';
 
-$sendTo = 'T-jack <contact@t-jack.fr>';
-
-// $sendTo = 'dwy <dwygaming@gmail.com>';
-
-$subject = 'Nouveau message du formulaire de contact - t-jack.fr';
-
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
+// array variable name => Text to appear in the email
+// array key => value     key : descripteur  -  value : valeur
+$fields = array('firstname' => 'Firstname', 'name' => 'Name', 'email' => 'Email', 'phone' => 'Phone', 'message' => 'Message'); 
 
 $okMessage = 'Message envoyé, merci ! A très bientôt !';
-
 $errorMessage = 'Erreur, merci de réessayer.';
 
+// à voir Plus tard
 $recaptchaSecret = '6LdYCcQZAAAAAPWljYt3ohRpCSas0sROJ4Y9okd-';
 
-
+// Test pour print les données entrées dans le formulaire apres un submit
+// print_r($_POST);
 
 // let's do the sending
-
-
-
+// Try : essaie de faire tout ce qui est dans les {} 
 try
-
 {
 
-    if (!empty($_POST)) {
-
-
+    if (!empty($_POST)) 
+    {  // ! = if not = si le formulaire n'est pas vide
 
         // validate the ReCaptcha, if something is wrong, we throw an Exception, 
-
-        // i.e. code stops executing and goes to catch() block
-
-        
-
-        if (!isset($_POST['g-recaptcha-response'])) {
-
-            throw new \Exception('ReCaptcha is not set.');
-
-        }
-
-
+        // i.e. code stops executing and goes to catch() block    
+        #if (!isset($_POST['g-recaptcha-response'])) 
+        #{
+        #    throw new \Exception('ReCaptcha is not set.');
+        #}
 
         // do not forget to enter your secret key in the config above 
-
         // from https://www.google.com/recaptcha/admin
-
         
+        //useless
+        //$recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret, new \ReCaptcha\RequestMethod\CurlPost());        
 
-        //$recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret, new \ReCaptcha\RequestMethod\CurlPost());
+		#$recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret);		
 
-        
+        // we validate the ReCaptcha field together with the user's IP address        
 
-		$recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret);
+        #$response = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
 
-		
+        #if (!$response->isSuccess()) 
+        #{
+        #    throw new \Exception('ReCaptcha was not validated.');
+        #}    
 
-        // we validate the ReCaptcha field together with the user's IP address
-
-        
-
-        $response = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
-
-
-
-
-
-        if (!$response->isSuccess()) {
-
-            throw new \Exception('ReCaptcha was not validated.');
-
-        }
-
-        
-
-        // everything went well, we can compose the message, as usually
-
-        
+        // everything went well, we can compose the message, as usually        
 
         $emailText = "You have new message from contact form\n=============================\n";
 
-
-
         foreach ($_POST as $key => $value) {
-
-
-
-            if (isset($fields[$key])) {
-
+            // si les $key des fields sont pas vides
+            if (isset($fields[$key])) 
+            {
+                // alors tu concatene  .=  la key et la value  
                 $emailText .= "$fields[$key]: $value\n";
-
             }
-
         }
 
-        
-
-
-
         $headers = array('Content-Type: text/plain; charset="UTF-8";',
-
             'From: ' . $from,
-
             'Reply-To: ' . $from,
-
             'Return-Path: ' . $from,
-
         );
-
-
-
+        // fonction mail official php
         mail($sendTo, $subject, $emailText, implode("\n", $headers));
-
-
-
+        // osef c'est pour du ajax
         $responseArray = array('type' => 'success', 'message' => $okMessage);
-
     }
-
 }
 
+// dans le cas d'une erreur tu fais ça
 catch (\Exception $e)
-
 {
-
     $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-
 }
 
-
-
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
+{
     $encoded = json_encode($responseArray);
-
-
-
     header('Content-Type: application/json');
-
-
-
     echo $encoded;
-
 }
 
-else {
-
+else 
+{
     echo $responseArray['message'];
-
 }
-
-
-
-
-
-
-
-
 ?>
